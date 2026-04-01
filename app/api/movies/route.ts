0c60d28d6b274b4bd9d6f5dd5f0edf051f8eee3b
Alexia Toma <alexia.g.toma.328@gmail.com>
2026-04-01 10:13:02 -0400

    added POST endpoint for movies

--- app/api/movies/route.ts
+++ app/api/movies/route.ts
@@ -1,4 +1,4 @@
-import { NextResponse } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 import { dbConnect } from "../../../lib/mongodb";
 import Movie from "../../../models/Movie";
 
@@ -13,6 +13,33 @@ export async function GET() {
     console.error("GET /api/movies error:", error);
     return NextResponse.json(
       { message: "Failed to fetch movies" },
+      { status: 500 }
+    );
+  }
+}
+
+// add a movie to the DB
+export async function POST(req: NextRequest) {
+  try {
+    await dbConnect();
+
+    const body = await req.json();
+
+    if (!body.title) {
+      return NextResponse.json(
+        { message: "Title is required" },
+        { status: 400 }
+      );
+    }
+
+    const newMovie = await Movie.create(body)
+
+    return NextResponse.json(newMovie, { status: 201 });
+
+  } catch (error) {
+    console.error("POST /api/movies error:", error);
+    return NextResponse.json(
+      { message: "Failed to create movie" },
       { status: 500 }
     );
   }
